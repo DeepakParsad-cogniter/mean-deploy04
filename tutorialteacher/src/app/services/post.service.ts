f2b1d2676dd7fb9f31c7ac9bce52ea37432d46d8
deepak <dparsad@cogniter.com>
2022-06-24 12:41:16 +0530

    url fixed

--- tutorialteacher/src/app/services/post.service.ts
+++ tutorialteacher/src/app/services/post.service.ts
@@ -8,7 +8,7 @@ import { timeStamp } from 'console';
 })
 export class PostService {
 	constructor(private http: HttpClient) {}
-	apibaseurl = "https://mean-deploy04.herokuapp.com:8081";
+	apibaseurl = "https://mean-deploy04.herokuapp.com:8081/";
 	// const headers = new HttpClient().set('content-type', 'application/json').set('Access-Control-Allow-Origin', '*');	// const headers = new HttpClient
 	pagedData : any;
 	status : any;
