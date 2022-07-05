ff5bf30b6c0ed26a0a1c706d540b9955b7b0d85a
deepak <dparsad@cogniter.com>
2022-07-05 13:00:13 +0530

    service bug

--- tutorialteacher/src/app/services/post.service.ts
+++ tutorialteacher/src/app/services/post.service.ts
@@ -9,7 +9,7 @@ import { timeStamp } from 'console';
 export class PostService {
 	constructor(private http: HttpClient) {}
 	apibaseurl = "https://localhost:8081/";
-	headers = new HttpClient().set('content-type', 'application/json').set('Access-Control-Allow-Origin', '*');	// const headers = new HttpClient
+	// headers = new HttpClient().set('content-type', 'application/json').set('Access-Control-Allow-Origin', '*');	// const headers = new HttpClient
 	pagedData : any;
 	status : any;
 	edituser(id:any)
