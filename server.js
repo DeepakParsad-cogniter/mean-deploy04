a01d5f32e0e6c4e922b6f35790b2d7e59b5934ad
deepak <dparsad@cogniter.com>
2022-07-06 12:39:34 +0530

    p

--- server.js
+++ server.js
@@ -49,9 +49,9 @@ app.use(cros({origin: '*'}));
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(express.static(__dirname + '/tutorialteacher/dist'));
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/tutorialteacher/dist/index.html'));
-// });
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/tutorialteacher/dist/index.html'));
+});
 /** Delete User */
 app.get('/edituser/:id', function (req, res) {
 	con.query("SELECT * FROM customers where id = '"+req.params.id+"'", function (err, result, fields) {
@@ -59,7 +59,7 @@ app.get('/edituser/:id', function (req, res) {
 		res.send(result);
 	});
 })
-app.get('/', function (req, res) {
+app.get('api/pagination', function (req, res) {
 	console.log("hi");
 	res.send({
 		message: "Profile fetched successfully!",
