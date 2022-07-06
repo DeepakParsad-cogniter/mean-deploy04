f49ceb6bbf8f3f0fb2dd4e3b8063e7958270f851
deepak <dparsad@cogniter.com>
2022-07-06 12:03:27 +0530

    k

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ const cros = require('cors');
 const path = require("path");
 const nodemailer = require('nodemailer');
 const Profile = require('./profile');
+const headers = new express.Router()
+
+headers.use(bodyParser.json());
+headers.use(bodyParser.urlencoded({ extended: false }));
+
+headers.use((req, res, next) => {
+   
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH,PUT, DELETE, OPTIONS"
+    ); next();
+});
 // var con = mysql.createConnection({
 // 	host: "",
 // 	user: "BIGmediaprinting",
